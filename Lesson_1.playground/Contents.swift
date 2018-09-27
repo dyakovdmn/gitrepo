6055485e5466592195e46e0b9880b77ffce82f07
dyakovdm <lildimm2012@Me.com>
2018-09-27 13:47:34 +0300

    swift/playground

---
+++ Lesson_1.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+
+var str: String = "Hello, playground"
+
+var number: Int = Int.max
+
+var numberStr = "6"
+var number1: Int = 0
+var numberD: Double = 1.55555
+
+number = Int(numberStr)!
+number = Int(numberD)
+numberStr = String(number)
+
+print(number)
+print(numberStr)
+
+var number12 : String = "Hello"
+
+
